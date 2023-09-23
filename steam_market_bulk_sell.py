3bd6bf3333d8c3ad6a26a34176b515391e29e785
shaongitt <shaongit@gmail.com>
2023-09-23 01:04:53 -0400

    fixed and updated all selenium issues

--- steam_market_bulk_sell.py
+++ steam_market_bulk_sell.py
@@ -7,23 +7,27 @@ from selenium.common.exceptions import TimeoutException
 from getpass import getpass
 import time
 
+
 username = input("Enter username: ")
 password = getpass()
 
 driver = webdriver.Firefox()
-driver.get("https://steamcommunity.com/login")
+driver.get("https://steamcommunity.com/login/home/?goto=login")
+time.sleep(10)
 
-username_field = driver.find_element_by_id("input_username")
-password_field = driver.find_element_by_id("input_password")
+username_field = driver.find_element(by=By.XPATH, value="//input[@type='text']")
+password_field = driver.find_element(by=By.XPATH, value="//input[@type='password']")
+
 
 username_field.send_keys(username)
 password_field.send_keys(password)
 
-sign_in_field = driver.find_elements_by_class_name("login_btn")[0]
+sign_in_field = driver.find_element(by=By.XPATH, value="//button[@type='submit']")
+
 sign_in_field.click()
 
 WebDriverWait(driver, 360).until(
-    expected_conditions.url_contains("https://steamcommunity.com/id/")
+    expected_conditions.url_contains("https://steamcommunity.com/profiles/")
 )
 
 inventory_url = driver.current_url + "/inventory/"
@@ -49,14 +53,14 @@ items_sold = 0
 while True:
     current_page = driver.execute_script("return g_ActiveInventory.m_iCurrentPage")
 
-    active_inventory_page = driver.find_element_by_id("active_inventory_page")
+    active_inventory_page = driver.find_element(by=By.ID, value="active_inventory_page")
     ctn = list(filter(lambda x: x.value_of_css_property("display") != "none",
-                      active_inventory_page.find_elements_by_class_name("inventory_ctn")))[0]
+                      active_inventory_page.find_elements(by=By.CLASS_NAME, value="inventory_ctn")))[0]
 
-    inventory_pages = ctn.find_elements_by_class_name("inventory_page")
+    inventory_pages = ctn.find_elements(by=By.CLASS_NAME, value="inventory_page")
 
     current_inventory_page = list(filter(lambda x: x.value_of_css_property("display") == "block", inventory_pages))[0]
-    item_holders = current_inventory_page.find_elements_by_class_name("itemHolder")
+    item_holders = current_inventory_page.find_elements(by=By.CLASS_NAME, value="itemHolder")
 
     continue_on_same_page = False
 
@@ -67,19 +71,21 @@ while True:
         holder.click()
 
         i = driver.execute_script("return g_ActiveInventory.selectedItem")
-        if i["classid"] == selected_item["classid"]:
+        if i["description"]["market_hash_name"] == selected_item["description"]["market_hash_name"]:
             sell_item = i
             print("found item on page")
             driver.execute_script("SellCurrentSelection()")
-            subscriber_agreement_checkbox = driver.find_element_by_id("market_sell_dialog_accept_ssa")
+            subscriber_agreement_checkbox = driver.find_element(by=By.ID, value="market_sell_dialog_accept_ssa")
             if not subscriber_agreement_checkbox.is_selected():
                 subscriber_agreement_checkbox.click()
-            price_text = driver.find_element_by_id("market_sell_currency_input")
+            price_text = driver.find_element(by=By.ID, value="market_sell_currency_input")
             price_text.send_keys(price)
-            sell_accept = driver.find_element_by_id("market_sell_dialog_accept")
+            sell_accept = driver.find_element(by=By.ID, value="market_sell_dialog_accept")
             sell_accept.click()
 
             WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.ID, "market_sell_dialog_ok"))).click()
+
+        
 
             try:
                 WebDriverWait(driver, 5).until(ec.element_to_be_clickable((By.XPATH, "//span[text()='OK']"))).click()
@@ -94,9 +100,14 @@ while True:
                 exit(0)
 
             # sell succeeded
-            if driver.find_element_by_id("market_headertip_itemsold").value_of_css_property("display") != "none":
+            if driver.find_element(by=By.ID, value="market_headertip_itemsold").value_of_css_property("display") != "none":
                 continue_on_same_page = True
                 break
+        else:
+            print(i["classid"])
+            print(selected_item["classid"])
+            print(i["class"])
+            print(i)
 
     # if end of inventory, break
     if current_page == driver.execute_script("return g_ActiveInventory.m_cPages") - 1:
@@ -105,4 +116,4 @@ while True:
 
     if not continue_on_same_page:
         driver.execute_script("g_ActiveInventory.NextPage()")
-        time.sleep(2)
+        time.sleep(3)
